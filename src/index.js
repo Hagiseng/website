f2e83c91c3de49c11c95de604bd55b460d604111
ishakhagi <ishak120201@gmail.com>
2022-01-31 15:16:20 +0100

    small change

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
